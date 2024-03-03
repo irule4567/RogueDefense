738c420f2ddd0b65d4491eda92c65544252c060d
irule4567 <kywilliford02@gmail.com>
2024-03-03 14:20:56 -0500

    Added placement of towers
    Added ability to place a basic test tower. Tower doesn't currently do anything yet.

--- Rouge Defense/MainGrid.gd
+++ Rouge Defense/MainGrid.gd
@@ -5,6 +5,10 @@ var GridWidth = 9
 var GridHeight = 5
 #var GridScaleFactor = 7 # How much to scale tiles by based on how much MainGrid was scaled
 var Dic = {}
+var Selected_tower
+var building
+
+
 
 func _ready():
 	for x in GridWidth:
@@ -20,3 +24,16 @@ func _process(delta):
 	if Dic.has(str(tile)):
 		#set_cell(1, tile, 1, Vector2i(0,0), 0)
 		print(Dic[str(tile)])
+	
+	if Input.is_action_pressed("Click") && building == true:
+		print("Place tower ", Selected_tower, " at position", Dic[str(tile)])
+		var new_tower = load("res://Test_Tower.tscn").instantiate()
+		new_tower.position = tile*Vector2i(108,108)+Vector2i(54,54)
+		building = false
+		get_node("Towers").add_child(new_tower, true)
+
+
+
+func _on_tower_button_pressed():
+	Selected_tower = "res://Test_Tower.tscn"
+	building = true
